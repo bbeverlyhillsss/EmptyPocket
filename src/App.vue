04cc7237fb796983ed89d5193814f9346b7fab7d
bbeverlyhillsss <bbeverlyhillsss@gmail.com>
2026-03-05 15:31:04 +0200

    add heroicons

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <h1 class="text-3xl font-bold text-red-600 underline decoration-double">
-    Якщо ти це бачиш червоним і з підкресленням — Tailwind працює!
-  </h1>
+
+
+
 </template>
+
+
 
 <script setup></script>
